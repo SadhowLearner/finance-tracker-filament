27a7392429582a27e01521313dd3419f417969c0
Jason4931 <emmanuel.jason2008@gmail.com>
2025-04-25 09:23:29 +0700

    resources

---
+++ app/Filament/Admin/Resources/SourceResource/Pages/CreateSource.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Admin\Resources\SourceResource\Pages;
+
+use App\Filament\Admin\Resources\SourceResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateSource extends CreateRecord
+{
+    protected static string $resource = SourceResource::class;
+}
